962feb623559468c94629222a1f6e3b859a36039
jsalvaguitian <jsalvaguitian@frba.utn.edu.ar>
2026-04-02 06:54:02 -0300

    inicios de c sharp

---
+++ practicas-net-core/practicas-net-core/Program.cs
@@ -0,0 +1,23 @@
+﻿//Practica 0: probando visual studio + concatenacion y tipos de variables
+
+int anio = Convert.ToInt16("2026"); //1era variable en posicion cero
+double doble = 2.5;
+string nombre = "Jesi";
+String nombres = "Jesica Belen";
+decimal decimales = 2.07M;
+float flotante = 8.0f;
+
+Console.WriteLine("Programacion web 3 "+ anio);
+
+Console.WriteLine("Taller Web 2 {0}", anio);
+
+int cuatri = 1;
+
+Console.WriteLine("Programacion Web 3 {0}, Cuatrimestre {1}", anio,cuatri);
+
+Console.WriteLine(nombres + " {0} {1} {2}", anio, doble, decimales);
+
+
+
+
+
